58d5cc5a60269b22c586c268a7bad915930ce105
Michal Rolinek <michalrolinek@gmail.com>
2020-03-25 01:05:01 +0100

    attempt at logging

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ with open('settings.json') as f:
     orig_settings = json.load(f)
 
 params = update_params_from_cmdline()
-os.makedirs(params.model_dir)
+os.makedirs(params.model_dir, exist_ok=True)
 
 input_path = os.path.join(params.input_dir, f'input_{params.folder_number}')
 orig_settings["ProblemGeneratorInputProviderSettings"]["InputFolderPath"] = input_path
 orig_settings["ProblemGenerationRunnerSettings"]["JsonBestTheoremFolder"] = params.model_dir
-orig_settings["LoggingSettings"]["FileLogPath"] = params.model_dir
+orig_settings["LoggingSettings"]["Loggers"][1]["FileLogPath"] = params.model_dir
 
 st = os.stat('GeoGen.MainLauncher')
 os.chmod('GeoGen.MainLauncher', st.st_mode | stat.S_IEXEC)
