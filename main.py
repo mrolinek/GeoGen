74ead64e2fd5a7954705a542c9f0696c22d0e5b5
Michal Rolinek <michalrolinek@gmail.com>
2020-11-24 23:23:17 +0100

    Attempt to set things up

--- main.py
+++ main.py
@@ -12,12 +12,15 @@ os.makedirs(params.model_dir, exist_ok=True)
 
 input_path = os.path.join(params.input_dir, f'input_{params.folder_number}')
 orig_settings["ProblemGeneratorInputProviderSettings"]["InputFolderPath"] = input_path
-orig_settings["ProblemGenerationRunnerSettings"]["JsonBestTheoremFolder"] = params.model_dir
-#orig_settings["LoggingSettings"]["Loggers"][1]["FileLogPath"] = params.model_dir
+orig_settings["ProblemGenerationRunnerSettings"]["JsonOutputFolder"] = params.working_dir
+orig_settings["Serilog"]["WriteTo"][1]["Args"]["Path"] = params.working_dir
 
 st = os.stat('GeoGen.MainLauncher')
 os.chmod('GeoGen.MainLauncher', st.st_mode | stat.S_IEXEC)
 
-os.system(f"./GeoGen.MainLauncher \'{json.dumps(orig_settings)}\'")
+with open(os.path.join(params.working_dir, "new_settings.json"), "w") as f:
+    json.dump(orig_settings, f)
+
+os.system(f"./GeoGen.MainLauncher {os.path.join(params.working_dir, 'new_settings.json')}")
 
 save_metrics_params({'done': 1}, params)
